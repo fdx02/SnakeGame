94ce9504305ec6ce2e107e54cf96b738f45cda29
fdx02 <franciscodelgado2002@gmail.com>
2025-06-11 17:15:33 -0300

    init
    done

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,6 @@ public class Main {
         SnakeGame snakeGame = new SnakeGame(boardWidth, boardHeight);
         frame.add(snakeGame);
         frame.pack();
+        snakeGame.requestFocus();
     }
 }
